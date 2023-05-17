42c08db3a4ac49da438d2346637cdbc5b515c569
Claudia Asti <asti@puzzle.ch>
2023-05-17 17:15:48 +0200

    Setup i18n with lit-localize

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:5173",
+    includeShadowDom: true,
   },
 });
--- cypress/e2e/my-element.cy.ts
+++ cypress/e2e/my-element.cy.ts
@@ -1,6 +1,6 @@
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("/");
-    cy.contains("Vite + Lit");
+    cy.visit("/index.html");
+    cy.contains("Willkommen bei Evento");
   });
 });
