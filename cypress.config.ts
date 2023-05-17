513f2944007cf75422b40c4d983b5268418b0e46
Claudia Asti <asti@puzzle.ch>
2023-05-17 18:07:22 +0200

    Use dedicated port for the dev server

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:5173",
+    baseUrl: "http://localhost:3000",
     includeShadowDom: true,
   },
 });
