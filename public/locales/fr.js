f6c5b10949f3ffe399b7a562ae5416a7f6774683
Claudia Asti <asti@puzzle.ch>
2023-06-05 10:59:03 +0200

    Add translations #522

--- public/locales/fr.js
+++ public/locales/fr.js
@@ -5,5 +5,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export const templates = {
+  s321998fa37fe89be: `© Direction de l'instruction publique et de la culture`,
   s3ba73c36f5c5710f: `Bienvenue chez Evento`,
+  sc874744fb4632e62: `Mentions légales`,
+  sd11a46c2e7022f58: `Impressum`,
+  sc265a7e29e120db0: `Evento`,
+  s6d8b7baee47fca15: `Menü`,
 };
