4568e2a84677be70a632ab1dfe0d03df36bf689f
Mathis Hofer <hofer@puzzle.ch>
2023-06-26 10:48:40 +0200

    Move apps to public dir & rename resize.ts to iframe.js

--- public/scripts/iframe.js
+++ public/scripts/iframe.js
@@ -1,8 +1,14 @@
+/**
+ * This script is executed within the apps' iframe and allows to setup
+ * aspects that have to be controlled from within the iframe.
+ */
+
+///// iframe resizing /////
+
 /*
  * Observes the size of the HTML element and triggers a message with the new height if the size changes.
  * Can be used to dynamically adjust the height of an iframe.
  */
-
 const resizeObserver = new ResizeObserver((entries) => {
   for (const entry of entries) {
     const height = entry.contentBoxSize
