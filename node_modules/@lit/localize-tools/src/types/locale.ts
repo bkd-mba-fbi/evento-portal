046a8115298a4ce743386025b3944d71101058e9
caebr <claudia@asti.ch>
2023-07-05 12:03:19 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@ec41081e707478336f9aa282360cee0c238c328d 🚀

--- node_modules/@lit/localize-tools/src/types/locale.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright 2021 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-/** Opaque type for locale IDs */
-export type Locale = string & {__TYPE__: 'Locale'};
