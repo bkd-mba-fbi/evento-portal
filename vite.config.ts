e7436565ffdb761c6ef37285389c92383d94b8b2
Mathis Hofer <hofer@puzzle.ch>
2023-07-06 15:38:47 +0200

    Add Vite middleware to fix case-insensitive EmberApps fetches #557

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 // vite.config.js
 import { defineConfig } from "vite";
 
+/**
+ * Adjust paths of /apps/EmberApps that are fetched with wrong case
+ * (due to ludicrous Windows' file system case insensitivity)
+ */
+const EventoEmberAppsCasePlugin = {
+  name: "evento-ember-apps-case",
+  configureServer(server) {
+    server.middlewares.use("/apps/EmberApps", (req, res, next) => {
+      req.url = req.url
+        .replace("components", "Components")
+        .replace("controllers", "Controllers")
+        .replace("templates", "Templates")
+        .replace("views", "Views")
+        .replace("//JSModules", "");
+      next();
+    });
+  },
+};
+
 export default defineConfig({
   server: {
     port: 3000,
   },
+  plugins: [EventoEmberAppsCasePlugin],
 });
