6fcee3869bb466443e58ffa83ef9221edf880eba
schefbi <41326409+schefbi@users.noreply.github.com>
2023-07-06 08:21:12 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@79943d799879a6717854b3ddbcbd680eacd5eb9c 🚀

---
+++ test/settings.example.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+/**
+ * This file contains the environment-specific configuration of the
+ * Evento Portal. Rename this file to `public/settings.js` and adjust
+ * its contents. For static configuration see `src/settings.ts`.
+ */
+window.eventoPortal = {
+  settings: {
+    /**
+     * Base URL of the Evento API
+     */
+    apiServer: "https://eventoapp-test.erz.be.ch/restApi",
+
+    /**
+     * Base URL of the OAuth provider
+     */
+    oAuthServer: "https://eventoapp-test.erz.be.ch/OAuth",
+
+    /**
+     * The OAuth client ID
+     */
+    oAuthClientId: "schoolWeb",
+  },
+};
