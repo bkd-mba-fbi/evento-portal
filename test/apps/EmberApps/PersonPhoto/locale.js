b8b5ceea1f17e5f7f6fba83bcaeb6c95cebc8eaa
caebr <claudia@asti.ch>
2023-08-14 13:01:34 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@1b3ee40bb56118d9346a3e59a544f8a8c12a6b62 🚀

---
+++ test/apps/EmberApps/PersonPhoto/locale.js
@@ -0,0 +1,12 @@
+// Rename this file to settings.js and adjust the settings
+window.personPhoto = window.personPhoto || {};
+window.personPhoto.locale = {
+
+      noWebcam: { de: "Keine Webcam vorhanden.", fr: "Pas de webcam disponible." } ,
+      neu:  {de: "Neu", fr: "Nouveau" } ,
+      aktuell:  {de: "Aktuell", fr: "Actuel" } ,
+      fotoAufnehmen: {de: "Foto aufnehmen", fr: "Prendre une photo" } ,
+      abbrechen: {de: "Abbrechen", fr: "Annuler" } ,
+      uebernehmen: {de: "Übernehmen", fr: "Valider" } ,
+
+};
