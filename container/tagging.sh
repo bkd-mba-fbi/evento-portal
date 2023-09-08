40ad0d0a3973a20da1c2b315b039f65220c660a8
Nils Kreienbühl <nils.kreienbuehl@protonmail.com>
2023-09-08 15:18:02 +0200

    Add Workflow for test-ci rename Containerfile to Dockerfile renamed shell scripts

--- container/tagging.sh
+++ container/tagging.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 # Author Nils Kreinebühl; kreienbuehl@puzzle.ch
 #
-# usage: ./tagging.sh 1.0.1 test
+# usage: ./retagging.sh
 
 set -o errexit
 
-declare -r VERSION=${1}
-declare -r ENVIRONMENT=${2}
+declare -r BASE_TAG="test"
+declare -r NEXT_TAG="production"
 declare -r IMAGE_NAME="evento-portal"
 
-/usr/bin/podman build -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:${ENVIRONMENT} .
+/usr/bin/podman pull ${IMAGE_NAME}:${BASE_TAG}
+/usr/bin/podman tag ${IMAGE_NAME}:${BASE_TAG} ${IMAGE_NAME}:${NEXT_TAG}
 
 # Bedag container registry
 # /usr/bin/podman login registry.ci-bedag.ch --creds username:password
