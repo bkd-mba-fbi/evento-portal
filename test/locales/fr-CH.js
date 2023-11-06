3ae88b13cd4b309c1682e86a761aacec8c9c9ec6
schefbi <41326409+schefbi@users.noreply.github.com>
2023-11-06 09:44:42 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@18857eda3ccacf5839b1856dedb4003f8ebae997 🚀

---
+++ test/locales/fr-CH.js
@@ -0,0 +1,47 @@
+// Do not modify this file by hand!
+// Re-generate this file by running lit-localize
+
+/* eslint-disable no-irregular-whitespace */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export const templates = {
+  s005058d82b713689: `Notes`,
+  s1a8b197f436e88ab: `Menu paramètres`,
+  s2ccb1c31fa62fea1: `Absences`,
+  s2e7964a3ed3ff4b2: `Saisir un remplacement`,
+  s2faf4f61874da789: `Rechercher des personnes et des institutions`,
+  s30329742fbb01b71: `Contrôle des présences`,
+  s321998fa37fe89be: `© Direction de l'instruction publique et de la culture`,
+  s41d88871e0b18f8b: `Cours et manifestations`,
+  s4c575b80febcc990: `Gérer les remplacements`,
+  s66fb201578f6f6c2: `Tutoriels vidéo`,
+  s676022c035fe65f5: `Effectuer un remplacement`,
+  s67749057edb2586b: `Se déconnecter`,
+  s6ab9bf010cfc8931: `Enseignement`,
+  s6d8b7baee47fca15: `Menu`,
+  s7459bbd6ff0ec70a: `Prestations`,
+  s784d82bd15c49b4a: `Paramètres`,
+  s79382e84e0818a6e: `Mon profil`,
+  s805a26293333f4d7: `Évaluer les absences`,
+  s885970c4ee8d21cc: `Fermer`,
+  s8bb0b618bf59b96e: `Détails de l'événement`,
+  s8e7e1013bc071156: `Saisie des notes`,
+  s918230658d993873: `Navigation mobile`,
+  s9b0191032da1cf5e: `Traiter les absences`,
+  sa37b83f4c60e63d3: `Excuser les absences en suspens`,
+  saac6cc64d5c17b4f: `Administration`,
+  sac1056b6664675ca: `Réserver des salles et des appareils`,
+  sb5ffc0faf2cf16da: `Formation continue interne`,
+  sbd4575edfbf5c84f: `Navigation principale`,
+  sc202079fc13ee45a: `Disciplines actuelles`,
+  sc217172426ab34d5: `Formation (continue)`,
+  sc265a7e29e120db0: `Evento`,
+  sc42d48a091dc3230: `Page d'accueil Evento`,
+  sc42d48a091dc3230: `Page d'accueil Evento`,
+  sc874744fb4632e62: `Mentions légales`,
+  sccb8c6ab6c0cf3e6: `Terminer le remplacement`,
+  scfa1631260cdd84a: `Tests et évalution`,
+  sd10fcf5b06af72f6: `Navigation service`,
+  sd11a46c2e7022f58: `Impressum`,
+  sec148f76974d9b06: `Saisir des détails d'inscription`,
+};
