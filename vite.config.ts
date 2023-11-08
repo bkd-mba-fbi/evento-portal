f1cd838e5a6be100f373837a9e357bc4b057c70b
Claudia Asti <asti@puzzle.ch>
2023-11-08 18:21:40 +0100

    Add SSL setup for BrowserStack & add BrowserStack documentation #69

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,10 @@
 // vite.config.js
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
-/**
- * Adjust paths of /apps/EmberApps that are fetched with wrong case
- * (due to ludicrous Windows' file system case insensitivity)
- 
-const EventoEmberAppsCasePlugin = {
-  name: "evento-ember-apps-case",
-  configureServer(server) {
-    server.middlewares
-      .use("/apps/EmberApps", (req, res, next) => {
-        req.url = req.url
-          .replace("components", "Components")
-          .replace("controllers", "Controllers")
-          .replace("templates", "Templates")
-          .replace("views", "Views")
-          .replace("//JSModules", "");
-        next();
-      })
-      .use("/apps/Noteneingabe", (req, res, next) => {
-        req.url = req.url.replace("components", "Components");
-        next();
-      });
-  },
-};
-*/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  //plugins: [EventoEmberAppsCasePlugin],
+  plugins: process.env.SSL ? [basicSsl()] : [],
 });
