4a89e859d011133a56f2a7b52651b041b4af2d44
Claudia Asti <asti@puzzle.ch>
2023-12-06 17:19:20 +0100

    Add delete button #562

--- src/components/Header/NotificationsDropdown.ts
+++ src/components/Header/NotificationsDropdown.ts
@@ -2,6 +2,7 @@ import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
 import { localized, msg } from "@lit/localize";
+import trashIcon from "../../assets/icons/trash.svg?raw";
 import { NotificationData } from "../../utils/fetch.ts";
 import { sanitize } from "../../utils/sanitize.ts";
 import { theme } from "../../utils/theme.ts";
@@ -40,33 +41,39 @@ export class NotificationsDropdown extends LitElement {
         border-bottom: 1px solid var(--bkd-func-bg-grey);
         align-items: center;
         padding: 1rem;
-      }
 
-      button {
-        font-weight: 400;
-        background-color: var(--bkd-func-bg-anthrazit);
-        color: var(--bkd-func-bg-white);
-        border-radius: 40px;
-        border: none;
-        padding: 0.5rem 1.5rem;
-        cursor: pointer;
-      }
+        button {
+          font-weight: 400;
+          background-color: var(--bkd-func-bg-anthrazit);
+          color: var(--bkd-func-bg-white);
+          border-radius: 40px;
+          border: none;
+          padding: 0.5rem 1.5rem;
+          cursor: pointer;
+        }
 
-      button:hover,
-      button:focus {
-        background-color: var(--bkd-func-bg-anthrazit-hover);
-      }
+        button:hover,
+        button:focus {
+          background-color: var(--bkd-func-bg-anthrazit-hover);
+        }
 
-      button:disabled {
-        cursor: default;
-        background-color: var(--bkd-func-bg-grey);
+        button:disabled {
+          cursor: default;
+          background-color: var(--bkd-func-bg-grey);
+        }
       }
 
       .notification {
-        display: flex;
-        justify-content: space-between;
-        padding: 1rem;
         border-bottom: 1px solid var(--bkd-func-bg-grey);
+        padding: 1rem;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        .content {
+          display: flex;
+          justify-content: space-between;
+        }
 
         .subject {
           padding-right: 1rem;
@@ -74,6 +81,15 @@ export class NotificationsDropdown extends LitElement {
 
         .body {
           text-align: right;
+        }
+
+        button {
+          align-self: end;
+          cursor: pointer;
+          border: none;
+          background: transparent;
+          padding: 0;
+          display: flex;
         }
       }
 
@@ -94,9 +110,14 @@ export class NotificationsDropdown extends LitElement {
     const sanitizedBody = sanitize(data.body);
 
     return html`<div class="notification">
-      <div class="subject">${unsafeHTML(sanitizedSubject)}</div>
-      <div class="body">${unsafeHTML(sanitizedBody)}</div>
-    </div>`;
+      <div class="content">
+        <div class="subject">${unsafeHTML(sanitizedSubject)}</div>
+        <div class="body">${unsafeHTML(sanitizedBody)}</div>
+      </div>
+      <button type="button" aria-label="${msg("Benachrichtigung löschen")}">
+        ${unsafeHTML(trashIcon)}
+      </button>
+    </div> `;
   }
 
   render() {
