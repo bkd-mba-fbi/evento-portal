fe8239ebb74137d1dd4ae108e3fc0bbd217f8ff2
hupf <mathis@fsfe.org>
2024-04-29 11:11:17 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@72283c8b1054708e8e5ae8db2040a76e067473ee 🚀

--- test/apps/EmberApps/AnmeldedetailsEinlesen/App/Controllers/application.js
+++
@@ -1,9 +0,0 @@
-define([
-    'ember',
-    'application'
-], function (ember, app) {
-    app.ApplicationController = ember.Controller.extend({
-        subscriptionDetails: null, // allow all Routes to access the subscriptionDetails
-        resetInputTextarea: false, // let the result Route reset the textArea when the details get sent
-    });
-});
