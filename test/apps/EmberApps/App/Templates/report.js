152ebd0785efc621ca4fc7de14e9574f4014dec3
schefbi <41326409+schefbi@users.noreply.github.com>
2024-12-19 13:27:51 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@7f0b0daea843947f71fe221cc702b3522cabd990 🚀

--- test/apps/EmberApps/App/Templates/report.js
+++
@@ -1,4 +0,0 @@
-﻿define(['ember', 'text!htmlTemplates/fullScreenLoading.html'], function (ember, loadingTemplate) {
-    ember.TEMPLATES['report'] = ember.Handlebars.compile(loadingTemplate);
-    ember.TEMPLATES['report.loading'] = ember.TEMPLATES['report'];
-});
