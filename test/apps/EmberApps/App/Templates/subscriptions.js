4e9a9aca0890a2fdd7b9b3f61310ee056a381f54
schefbi <41326409+schefbi@users.noreply.github.com>
2024-12-19 15:39:29 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@64472b11d9349c46b8998a874392a1ae48078361 🚀

--- test/apps/EmberApps/App/Templates/subscriptions.js
+++
@@ -1,3 +0,0 @@
-﻿define(['ember', 'text!htmlTemplates/subscriptions.html'], function (ember, subscriptionsTemplate) {
-    ember.TEMPLATES['subscriptions'] = ember.Handlebars.compile(subscriptionsTemplate);
-});
