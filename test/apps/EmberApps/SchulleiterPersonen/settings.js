3b90d7c72578bd3035691f57026786aa95ca3dd1
hupf <mathis@fsfe.org>
2025-03-25 15:48:57 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@d0e5968b810c519271e19f54f4625b2e4a4cff66 🚀

---
+++ test/apps/EmberApps/SchulleiterPersonen/settings.js
@@ -0,0 +1,11 @@
+﻿define([
+    'main_settings',
+    'constants'
+], function (mainSettings, constants) {
+    var settings = {
+        // module specific settings
+        applicationScope: 'NG',
+    };
+    settings.__proto__ = mainSettings;
+    return settings;
+});
