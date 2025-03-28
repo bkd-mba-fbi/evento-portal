7c1431607e885ddd66b3c1f7668a28e981837d47
hupf <mathis@fsfe.org>
2025-03-28 10:37:10 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@52dfb301c89006f72f34b0ba38fb4425f9411555 🚀

--- test/apps/EmberApps/App/Templates/subscriptionPayment.js
+++
@@ -1,3 +0,0 @@
-﻿define(['ember', 'text!htmlTemplates/subscriptionPayment.html'], function (ember, subscriptionPayment) {
-    ember.TEMPLATES['subscriptionPayment'] = ember.Handlebars.compile(subscriptionPayment);
-});
