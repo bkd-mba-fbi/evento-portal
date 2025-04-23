ea9a11aba979595731f5c1895390c31e7b952981
schefbi <41326409+schefbi@users.noreply.github.com>
2025-04-23 14:39:53 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@081ebd184db071a2c88e0aa17b91d3761bb16ea4 🚀

---
+++ test/apps/EmberApps/SchulleiterPersonen/uiSettings.js
@@ -0,0 +1,10 @@
+﻿define([
+    'ember',
+    'main_uiSettings'
+], function (ember, mainUiSettings) {
+    var uiSettings = {
+        // module specific settings
+    };
+    uiSettings.__proto__ = mainUiSettings;
+    return uiSettings;
+});
