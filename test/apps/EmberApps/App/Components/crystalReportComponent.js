57f0fce07ba7eaf818ac2311297f839eb5005ea9
schefbi <41326409+schefbi@users.noreply.github.com>
2025-05-02 09:39:27 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@5711214a616fdc2483fc7a49dd87be23210b85ef 🚀

---
+++ test/apps/EmberApps/App/Components/crystalReportComponent.js
@@ -0,0 +1,29 @@
+﻿define([
+    'ember',
+    'api',
+    'guiHelpers',
+    'icons'
+], function (ember, api, guiHelpers, icons) {
+    ClxApp.CrystalReportComponent = ember.Component.extend({
+        tagName: 'a',
+        attributeBindings: ['target'],
+
+        didInsertElement: function() {
+            var that = this;
+            var reportContext = this.get('reportContext');
+            var reportId = this.get('reportId');
+            var dataKey = this.get('dataKey');
+            var caption = this.get('caption');
+            
+            api.getAvailableCrystalReports(reportContext, [reportId], [dataKey], function (result) {
+                if (result && result.length > 0 && (!that.get('storedPdf') || result[0].AllowedKeys.indexOf(dataKey) > -1)) {
+                    that.$()
+                        .addClass(guiHelpers.classes.report.pdfReport)
+                        .addClass(guiHelpers.classes.dialog.textIcon)
+                        .attr('href', '#/reports/' + reportContext + '/' + result[0].Id + '/' + dataKey)
+                        .html(icons.pdf + (caption ? caption : result[0].Title));
+                }
+            });
+        }
+    });
+});
