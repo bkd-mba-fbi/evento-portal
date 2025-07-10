bdf0c6c6f7911afeb17f16b0ffcfda972e2361d0
caebr <claudia@asti.ch>
2025-07-10 14:06:45 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@492cee557386c2516f0020f6b32fe634103e4210 🚀

---
+++ test/apps/EmberApps/App/application.js
@@ -0,0 +1,61 @@
+﻿define([
+    'ember'
+], function (ember) {
+
+    var app = {
+        rootElement: '#CLX_Root',
+        Router: ember.Router.extend(),
+        automaticTemplateLoading: true
+    };
+
+    // tracking with piwik, if the piwik object has been defined
+    if (typeof _paq === 'object')
+        app.Router.reopen({
+            notifyPiwik: function () {
+                _paq.push(['trackPageView']);
+            }.on('didTransition')
+        });
+
+    // tracking with google analytics, if the ga method has been defined
+    if (typeof ga === 'function')
+        app.Router.reopen({
+            notifyGoogleAnalytics: function () {
+                return ga('send',
+                    'pageview',
+                    {
+                        'page': this.get('url'),
+                        'title': this.get('url')
+                    });
+            }.on('didTransition')
+        });
+
+    // initialize ember reopens
+    ember.Route.reopen({
+        beforeModel: function (transition) {
+            if (!app.automaticTemplateLoading)
+                return undefined;
+
+            return new ember.RSVP.Promise(function (resolve, reject) {
+                var modules = [];
+                $.each(transition.handlerInfos,
+                    function () {
+                        if (this.name
+                            .indexOf('.index') ===
+                            -1 &&
+                            this.name !== 'index' &&
+                            this.name !== 'application' &&
+                            this.name.indexOf('EntryPoint') === -1) {
+                            modules.push('templates/' + this.name);
+                        }
+                    });
+
+                require(modules,
+                    function () {
+                        resolve();
+                    });
+            });
+        }
+    });
+
+    return app;
+});
