3971a3762d41579b291a3e11d9d1fa1bd1425784
schefbi <41326409+schefbi@users.noreply.github.com>
2025-07-16 15:24:30 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@e6edd1b62800f48a945e8a30f446b9d45b785dfb 🚀

---
+++ test/apps/EmberApps/App/Components/addressLabelComponent.js
@@ -0,0 +1,39 @@
+﻿define([
+    'jquery',
+    'ember'
+], function ($, ember) {
+    ClxApp.AddressLabelComponent = ember.Component.extend({
+        tagName: 'pre',
+        classNames: ['address'],
+
+        didInsertElement: function () {
+            this.$().text(this.getText());
+        },
+
+        getText: function() {
+            var address = this.get('address');
+            var text = '';
+            if (address.Company) {
+                text += address.Company;
+                text += '\r\n';
+            }
+            if (address.Department) {
+                text += address.Department;
+                text += '\r\n';
+            }
+            text += address.FormOfAddress + ' ' + (address.FirstName ? address.FirstName + ' ': '') + address.LastName;
+            text += '\r\n';
+            text += address.AddressLine1;
+            text += '\r\n';
+            if (address.AddressLine2) {
+                text += address.AddressLine2;
+                text += '\r\n';
+            }
+            text += address.Zip + ' ' + address.Location;
+            text += '\r\n';
+            text += address.Country;
+
+            return text;
+        }
+    });
+});
