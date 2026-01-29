2d21b7a22c1bf097b6cd2073263deef5b0fb59af
schefbi <41326409+schefbi@users.noreply.github.com>
2026-01-29 12:32:57 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@c8fe5c0a66e6c3c887f0442fc0de124d51ade7ca 🚀

--- test/apps/EmberApps/App/Templates/components/subscriptionDetailGroupComponent.js
+++
@@ -1,3 +0,0 @@
-﻿define(['ember', 'text!htmlTemplates/components/subscriptionDetailGroupComponent.html'], function (ember, subscriptionDetailComponentTemplate) {
-    ember.TEMPLATES['components/subscription-detail-group'] = ember.Handlebars.compile(subscriptionDetailComponentTemplate);
-});
