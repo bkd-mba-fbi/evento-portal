2f846680307749aae1ccd05604492cb4bd053c48
schefbi <41326409+schefbi@users.noreply.github.com>
2026-02-19 13:27:48 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@3bc567d706713ac831d25999da4038e28529f2d9 🚀

--- test/apps/EmberApps/App/Templates/sdFile.js
+++
@@ -1,4 +0,0 @@
-﻿define(['ember', 'text!htmlTemplates/fullScreenLoading.html'], function (ember, loadingTemplate) {
-    ember.TEMPLATES['sdFile'] = ember.Handlebars.compile(loadingTemplate);
-    ember.TEMPLATES['sdFile.loading'] = ember.TEMPLATES['sdFile'];
-});
