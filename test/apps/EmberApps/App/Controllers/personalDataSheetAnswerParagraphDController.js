181385369f2df72582ea2fd6d0ecaa4fd53837c4
schefbi <41326409+schefbi@users.noreply.github.com>
2026-03-04 10:14:21 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@d330826c7b46f1508980bf3d94ef90861f2cee51 🚀

---
+++ test/apps/EmberApps/App/Controllers/personalDataSheetAnswerParagraphDController.js
@@ -0,0 +1,44 @@
+﻿define([
+    'jquery',
+    'ember',
+    'uiSettings'
+], function ($, ember, uiSettings) {
+    ClxApp.PersonalDataSheetAnswerParagraphDController = ember.Controller.extend({
+        CityIdLFS: function (key, value) {
+            return this.handleEntry(value, 'CityIdLFS');
+        }.property('model.CityIdLFS'),
+
+        KindOfIndependence: function (key, value) {         
+            return this.handleEntry(value, 'KindOfIndependence');
+        }.property('model.KindOfIndependence'),
+
+        DateFrom: function (key, value) {
+            return this.handleEntry(value, 'DateFrom');
+        }.property('model.DateFrom'),
+
+        DateTo: function (key, value) {
+            return this.handleEntry(value, 'DateTo');
+        }.property('model.DateTo'),
+
+        Cities: function () {
+            return this.get('parentController').get('CitiesFiltered');
+        }.property('parentController.CitiesFiltered'),
+   
+        IsCity: function () {
+            return this.get('model.IsCity');
+        }.property('model.IsCity'),
+
+        handleEntry: function (value, propertyName) {
+            if (value !== undefined) {
+                this.set('model.' + propertyName, value);
+                ember.run.debounce(this, this.sendAnswerParagraph, uiSettings.defaultDebouncing);
+            }
+
+            return this.get('model.' + propertyName);
+        },
+
+        sendAnswerParagraph: function() {
+            this.get('parentController').send('answerParagraphD');
+        }
+    });
+});
