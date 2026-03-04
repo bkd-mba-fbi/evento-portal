bfb170144f4a766468ae151e4ee2330a32b2ed6c
schefbi <41326409+schefbi@users.noreply.github.com>
2026-03-04 10:13:25 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@d330826c7b46f1508980bf3d94ef90861f2cee51 🚀

--- test/apps/Noteneingabe/Framework/regexHelpers.js
+++
@@ -1,8 +0,0 @@
-﻿define([], function () {
-    var regexHelpers = {
-        escapeRegExp: function(str) {
-            return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
-        }
-    };
-    return regexHelpers;
-});
