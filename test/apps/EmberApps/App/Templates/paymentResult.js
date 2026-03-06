0f4cbdd3efe674060db831a119815c9c92f42086
schefbi <41326409+schefbi@users.noreply.github.com>
2026-03-06 15:33:12 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@29fdb43adac5770e05a7160dc08436ae81ccc30e 🚀

--- test/apps/EmberApps/App/Templates/paymentResult.js
+++
@@ -1,3 +0,0 @@
-﻿define(['ember', 'text!htmlTemplates/paymentResult.html'], function (ember, paymentResultTemplate) {
-    ember.TEMPLATES['paymentResult'] = ember.Handlebars.compile(paymentResultTemplate);
-});
