b22cdbf8fe4f985705f6ed60af86dfc07517c749
schefbi <41326409+schefbi@users.noreply.github.com>
2026-03-17 07:43:35 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@d77843334d90276a7549c21e8a9a158629955b2a 🚀

---
+++ test/apps/Noteneingabe/App/Views/subscriptionInputView.js
@@ -0,0 +1,49 @@
+﻿define([
+    'framework',
+    'guiHelpers',
+    'keyboard'
+], function (framework, guiHelpers, keyboard) {
+    ClxApp.SubscriptionInputView = framework.OverlayView.extend({
+        usePrintButton: false,
+        dragHandle: '.flexDialogHeader',
+
+        hasBeenInvalidated: function () {
+            var invalidated = this.get('controller.invalidated');
+            if (invalidated && this.$())
+                this.$().find('.validationError').addClass('showError');
+        }.observes('controller.invalidated'),
+
+        destroyWidgetsRepeat: function () {
+            // HACK: this is the only way to do it at the moment. There is a search-Widget in the background listening to those events -> reregister here (on close)
+            $('#eventoSearch').eventosearch('refreshAfterDialogClosed');
+        },
+
+        loadWidgetsRepeat: function() {
+            this._super();
+
+            // HACK: this is the only way to do it at the moment. There is a search-Widget in the background listening to those events
+            keyboard.unregisterShortcuts('Up');
+            keyboard.unregisterShortcuts('Down');
+            // unfortunately when reloading the page the search will be loaded later...
+            setTimeout(function() { 
+                keyboard.unregisterShortcuts('Up');
+                keyboard.unregisterShortcuts('Down');
+            }, 1000);
+
+            var selectedWorkProgress = this.get('model.SelectedWorkProgress');
+            if (selectedWorkProgress) {
+                guiHelpers.addContextMenuToButton(this.$().find('#infoButton'),
+                    function (contextMenu) {
+                        contextMenu.text(selectedWorkProgress.Note);
+                    });
+            }
+        },
+
+        baseTransition: function () {
+            // here transition back to subscription
+            this.get('controller').send('closeRoute');
+        }
+    });
+});
+
+
---
+++ test/apps/Noteneingabe/router.js
@@ -0,0 +1,135 @@
+﻿define([
+    'ember',
+    'application',
+    'api',
+    'settings',
+    'urlHelpers',
+    'arrayHelpers',
+    'jquery',
+    'translate',
+    'dateHelpers',
+], function (ember, app, api, settings, urlHelpers, arrayHelpers, jquery, translate, dateHelpers) {
+    app.Router = ember.Router.extend();
+    app.Router.map(function () {
+        this.route('grading', { path: '/grading/:idEvent' });
+        this.route('statistic', { path: '/statistic/:idEvent' });
+    });
+
+    app.IndexRoute = ember.Route.extend({
+        beforeModel: function () { 
+            var idEvent = urlHelpers.getUrlParameter(settings.querystringName);
+            if (idEvent)
+                this.transitionTo('grading', idEvent);
+        },
+        model: function () {
+            return app.loadAllEvents();
+        },
+        afterModel: function (model) {
+            
+            let availableToBeGraded = this.sortData(model.Events);
+
+            availableToBeGraded = availableToBeGraded.map(item => {
+                if(item.StatusDate){
+                    const date = dateHelpers.formatDate(item.StatusDate);
+                    ember.set(item, 'StatusDate', date);
+                }
+                
+                return item;
+            })
+
+            ember.set(model, 'toGrade', availableToBeGraded); 
+        },
+
+        sortData: function(data) {
+            return data.sort(function (a, b) {
+                var dateA = a.StatusDate || '9999-12-31T23:59:59';
+                var dateB = b.StatusDate || '9999-12-31T23:59:59';
+                return new Date(dateA) - new Date(dateB) 
+                || a.Designation.localeCompare(b.Designation);
+            });
+        }
+    });
+
+    app.GradingRoute = ember.Route.extend({
+        model: function (params) {
+            return app.loadGradingModels(params);
+        },
+        afterModel: function (model) {
+            var settingsDetails = settings.grading.adColumns.concat(settings.grading.adRange);
+            if (settingsDetails === undefined)
+                settingsDetails = [];
+
+            var modelDetails = model.SubscriptionDetails;
+            if (modelDetails === undefined)
+                modelDetails = [];
+
+            var subscriptionDetails = arrayHelpers.intersectArrays(modelDetails, settingsDetails, function (subDet, setDet) {
+                return subDet.IdAnmeldeVSS === setDet;
+            });
+            ember.set(model, 'SubscriptionDetails', subscriptionDetails);
+            if (subscriptionDetails.length === 0 && model.Event.Scale === undefined) {
+                ember.set(model, 'hasNothingToShow', true);
+            }
+        }
+    });
+
+    app.StatisticRoute = ember.Route.extend({
+        model: function (params) {
+            return app.loadGradingModels(params);
+        },
+        afterModel: function (model) {
+            if (!model)
+                return;
+            if (model.Event.Scale === undefined) {
+                ember.set(model, 'hasNothingToShow', true);
+            }
+        }
+    });
+
+    app.loadGradingModels = function(params) {
+        var result = {
+            Event: api.ember.getEvent(params.idEvent).then(function (event) {
+                return new ember.RSVP.Promise(function (resolve) {
+                    if (!event || !event.GradingScaleId) {
+                        resolve(event);
+                        return;
+                    }
+
+                    api.ember.getGradingScale(event.GradingScaleId).then(function (scale) {
+                        event.Scale = scale;
+                        resolve(event);
+                    });
+                });
+            }),
+            SubscriptionDetails: api.ember.getSubscriptionDetailsByEvent(params.idEvent),
+            GradingItems: api.ember.getGradingItems(params.idEvent),
+        }; 
+
+        var unregisteredStudents = Promise.all([api.ember.getGradingItemsForUnregisteredStudents(params.idEvent), result.SubscriptionDetails])
+            .then(([unregisteredStudents, subscriptionDetails]) => {
+                var isUnregisteredStudentsEnabled = unregisteredStudents != null;
+                result.IsUnregisteredStudentsEnabled = isUnregisteredStudentsEnabled;
+
+                if (isUnregisteredStudentsEnabled) {
+                    var unregisteredStudentsTransformed = api.helpers.transformUnregisteredStudents(unregisteredStudents, subscriptionDetails, settings.grading.adColumns, settings.grading.adRange);
+                    return unregisteredStudentsTransformed;
+                } else {
+                    return [];
+                }
+            });
+
+        result.UnregisteredStudents = unregisteredStudents;
+        result.IsUnregisteredStudentsEnabled = false;
+
+        return new ember.RSVP.hash(result).then(v => {
+            v.IsUnregisteredStudentsEnabled = result.IsUnregisteredStudentsEnabled;
+            return v;
+        });
+    };
+
+    app.loadAllEvents = function(){
+        return new ember.RSVP.hash({
+            Events: api.ember.getEventsToBeGraded() 
+        });
+    }
+});
