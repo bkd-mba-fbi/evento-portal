bd60bda1be3886cb054feba66cd1da1d828cf7d9
hupf <mathis@fsfe.org>
2026-03-26 12:23:58 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@8830fa83949f70a1d2ba447625a5b723fab34418 🚀

---
+++ test/apps/EmberApps/App/Components/adaptableTextBoxComponent.js
@@ -0,0 +1,22 @@
+﻿define([
+    'application',
+    'jquery',
+    'ember',
+    'guiHelpers'
+],
+    function(app, $, ember, guiHelpers) {
+        app.AdaptableTextBoxComponent = ember.Component.extend($.extend({},
+            guiHelpers.adaptableTextBoxComponentLogic,
+            {
+                tagName: 'div',
+                classNames: ['textBox'],
+                didInsertElement: function() {
+                    var element = this.$();
+                    this.initializeInputControl(element);
+                },
+                updateControlModel: function(value) {
+                    this.set('value', (value || '').trim());
+                }
+            }
+        ));
+    });
