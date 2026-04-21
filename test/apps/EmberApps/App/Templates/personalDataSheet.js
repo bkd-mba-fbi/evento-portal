60bb2513bab255ecd6212429180ea1826d72a9c6
hupf <mathis@fsfe.org>
2026-04-21 08:27:38 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@e564fe924f2ee5df9de36b6b48a97d3177a458f3 🚀

--- test/apps/EmberApps/App/Templates/personalDataSheet.js
+++
@@ -1,3 +0,0 @@
-﻿define(['ember', 'text!htmlTemplates/personalDataSheet.html'], function (ember, personalDataSheetTemplate) {
-    ember.TEMPLATES['personalDataSheet'] = ember.Handlebars.compile(personalDataSheetTemplate);
-});
