8272cbb858e58bff5c0e51c2ee75fd4fd84164e2
WalidBoul <walid.boulhazayez@be.ch>
2026-04-30 09:19:35 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@3aef350324f389cf360873a42f03c6bc158a216c 🚀

--- test/apps/EmberApps/App/Templates/components/formControlComponent.js
+++
@@ -1,3 +0,0 @@
-﻿define(['ember', 'text!htmlTemplates/components/formControlComponent.html'], function (ember, formControlComponentTemplate) {
-    ember.TEMPLATES['components/form-control'] = ember.Handlebars.compile(formControlComponentTemplate);
-});
