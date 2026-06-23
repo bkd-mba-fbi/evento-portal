b3fb1bb88604b6f1ecc2c16a29763966300a80ec
schefbi <41326409+schefbi@users.noreply.github.com>
2026-06-23 11:57:48 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@a0d087271b45a20dd193c3ebee0f3b62ad85eea3 🚀

---
+++ test/apps/EmberApps/App/Templates/components/onlaWorkProgressComponent.js
@@ -0,0 +1,3 @@
+﻿define(['ember', 'text!htmlTemplates/components/onlaWorkProgressComponent.html'], function (ember, onlaWorkProgressComponentTemplate) {
+    ember.TEMPLATES['components/onla-work-progress'] = ember.Handlebars.compile(onlaWorkProgressComponentTemplate);
+});
---
+++ test/apps/EmberApps/App/Templates/sdFile.js
@@ -0,0 +1,4 @@
+﻿define(['ember', 'text!htmlTemplates/fullScreenLoading.html'], function (ember, loadingTemplate) {
+    ember.TEMPLATES['sdFile'] = ember.Handlebars.compile(loadingTemplate);
+    ember.TEMPLATES['sdFile.loading'] = ember.TEMPLATES['sdFile'];
+});
