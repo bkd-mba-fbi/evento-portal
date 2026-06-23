de093040d5bbf04ce13cf5b81f56db44d311d762
hupf <mathis@fsfe.org>
2026-06-23 12:06:32 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@6a2ff81960b756800a17c78a3f0eac8bfdaaef52 🚀

---
+++ test/apps/EmberApps/App/Components/iconButtonComponent.js
@@ -0,0 +1,36 @@
+﻿define([
+    'jquery',
+    'ember',
+    'application',
+    'icons',
+    'keyboard'
+], function ($, ember, app, icons, keyboard) {
+    app.IconButtonComponent = ember.Component.extend({
+        tagName: 'button',
+        type: 'button',
+
+        attributeBindings: ['title', 'type', 'disabled'],
+
+        didInsertElement: function() {
+            if (this.get('icon') !== undefined)
+                this.$().html(icons[this.get('icon')]);
+
+            if (this.get('shortcut') !== undefined)
+                keyboard.registerShortcut(this.get('shortcut'), this.$());
+        },
+
+        click: function () {
+            if (!this.get('isWorking')) {
+                this.sendAction();
+            }
+        },
+
+        isWorkingChanged: ember.observer('isWorking',
+            function() {
+                if (this.get('isWorking'))
+                    this.$().html(icons.loadingAnimation);
+                else
+                    this.$().html(icons[this.get('icon')]);
+            })
+    });
+});
