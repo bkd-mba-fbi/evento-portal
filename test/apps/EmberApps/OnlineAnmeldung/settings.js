ebae759975b1bc682904d5cd7ea02d05aaae7d24
schefbi <41326409+schefbi@users.noreply.github.com>
2026-06-24 14:39:50 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@fa8317726bd5c9d0a468dcf4517e97267558d8cc 🚀

---
+++ test/apps/EmberApps/OnlineAnmeldung/settings.js
@@ -0,0 +1,15 @@
+﻿define([
+    'ember',
+    'main_settings',
+    'constants'
+], function (ember, mainSettings, constants) {
+    var settingsClass = ember.Object.extend(mainSettings);
+    var settings = {
+        // module specific settings
+        applicationScope: constants.applicationScope.onla,
+        subscriptionsSearchDefinition: 'ONLA_Suchliste_Anmeldungen',
+        eventSearchDefinition: 'ONLA_Suchliste_Anmeldbare_STG'
+    };
+    var inheritedSettingsClass = settingsClass.extend(settings);
+    return inheritedSettingsClass.create();
+});
