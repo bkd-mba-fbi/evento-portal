cc2d781932b905be42630b1717de9fd756e2b629
schefbi <41326409+schefbi@users.noreply.github.com>
2026-06-24 10:23:58 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@5dc9f0a27be21f85a108efbccdaa9b967c5f109a 🚀

--- test/apps/Noteneingabe/App/Templates/components/unregisteredStudentsComponent.js
+++
@@ -1,4 +0,0 @@
-﻿define(['ember', 'text!htmlTemplates/components/unregisteredStudentsComponent.html'], function (ember, templateString) {
-    console.log('unregistered-students templated compiling');
-    ember.TEMPLATES['components/unregistered-students'] = ember.Handlebars.compile(templateString);
-});
