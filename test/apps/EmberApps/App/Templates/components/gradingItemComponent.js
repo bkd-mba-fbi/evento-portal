bffc6666fcc19ddafd49e387b93c70aff823a4de
hupf <mathis@fsfe.org>
2026-07-28 08:40:10 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@4bfc4a0a23b19c56539fc08b49f8e00d1ca03ad3 🚀

---
+++ test/apps/EmberApps/App/Templates/components/gradingItemComponent.js
@@ -0,0 +1,3 @@
+﻿define(['ember', 'text!htmlTemplates/components/gradingItemComponent.html'], function (ember, gradingItemComponent) {
+    ember.TEMPLATES['components/grading-item'] = ember.Handlebars.compile(gradingItemComponent);
+});
