86cca0016e44f8ce04f36be0c2c43e3bfd4ff595
hupf <mathis@fsfe.org>
2026-07-28 08:38:51 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/evento-portal@bc74ef26a6498c805fe4fba8e98a0b2a601774dd 🚀

---
+++ test/apps/Noteneingabe/App/Components/flotGraphicComponent.js
@@ -0,0 +1,49 @@
+﻿define([
+    'jquery',
+    'ember',
+    'flotHelpers',
+    'componentCaller',
+    'guiHelpers'
+], function ($, ember, flotHelpers, componentCaller, guiHelpers) {
+    ClxApp.FlotGraphicComponent = ember.Component.extend({
+        classNames: ['flotThumb'],
+
+        didInsertElement: function () {
+            var element = this.$();
+            var that = this;
+            ember.run.scheduleOnce('afterRender',
+                function() {
+                    that.createPlot(element, that.get('legendSelector'));
+
+                    element
+                        .css('cursor', 'pointer')
+                        .click(function() {
+                            var overlayContainer;
+                            if (!that.get('clickable')) return;
+                            componentCaller.openOverlayDialog(that,
+                                true,
+                                function (dialogBody) {
+                                    dialogBody.css('height', '100%');
+                                    overlayContainer = guiHelpers.getDiv(guiHelpers.classes.flot.flotFull)
+                                        .appendTo(dialogBody);
+                                },
+                                function(header) {
+                                    header.append(guiHelpers.getDiv(guiHelpers.classes.flot.flotLegend)
+                                        .attr('id', 'flot-legend-overlay'));
+
+                                    that.createPlot(overlayContainer, '#flot-legend-overlay');
+                                });
+                        });
+                });
+        },
+
+        createPlot: function(container, legendSelector) {
+            flotHelpers.createPlot(container,
+                this.get('series'),
+                this.get('ticks'),
+                $(legendSelector),
+                this.get('xAxisLabel'),
+                this.get('yAxisLabel'));
+        }
+    });
+});
